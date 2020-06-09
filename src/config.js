a9773e42db80b413cecc631a76a42f303f79f4a8
khains <44432994+khains@users.noreply.github.com>
2020-06-09 17:10:11 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     rootPath : 
-        process.env.REACT_APP_STATE === "localhost" ? "http://localhost:6969" 
+        process.env.REACT_APP_STATE === "localhost" ? "https://kshop-server.herokuapp.com" 
             : "https://kshop-server.herokuapp.com"
 };
